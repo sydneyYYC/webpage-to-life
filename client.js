e38abd216227344603d764dd8ed39e57dc390571
Sydney Bruce <sydneybruce20@gmail.com>
2023-10-30 14:04:42 -0700

    mobile nav working

--- client.js
+++ client.js
@@ -3,3 +3,32 @@
 //render on HTML page using JS
 const aboutHeading = "The Story";
 let aboutRender = document.querySelector("#about-heading").innerHTML = aboutHeading;
+
+
+
+// image gallery
+
+const images = {
+
+}
+
+// list all images in an object as strings
+
+// use map to iterate through each object and render using `${}` TWICE - thanks chad :) 
+
+// render using document query selector
+
+// 
+
+
+// mobile nav 
+function myNav() {
+  const x = document.getElementById("nav-links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+// connect myNav function to menu icon
+document.querySelector("#nav-toggle").addEventListener("click", myNav)
