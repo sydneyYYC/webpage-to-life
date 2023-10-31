a3c3e0a266daef7e91f40311746f63d32fa65bff
Sydney Bruce <sydneybruce20@gmail.com>
2023-10-30 21:23:00 -0700

    added images and loop debugging gallery display

--- README.md
+++ README.md
@@ -7,8 +7,17 @@
 js function for mobile nav toggle 
 https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar - mobile nav bar help
 
+mobile nav found on line 11 
+
+js function loop that creates images 
+https://www.geeksforgeeks.org/how-to-display-images-from-an-array-in-javascript/
+
 issue with nav menu disappearing and not reappearing after nav has been clicked . 
 ended up erasing the href to javascript in html - talked to tony and he said it  was just a different way to connect / call the function but its not the best way- better to add a eventListener in the js instead. leftover it was causing issues w the toggle. 
 
 
-now im having issues where the toggle is automatically refreshing the page and resetting the page when clicked
+now im having issues where the toggle is automatically refreshing the page and resetting the page when clicked -- fixed , had added a link to the nav which was refreshing the page , making nav refresh when clicked 
+
+still running into bug where if menu is closed in mobile and then stretched links will disappear WILL FIX LATER
+
+
--- client.js
+++ client.js
@@ -5,12 +5,32 @@ const aboutHeading = "The Story";
 let aboutRender = document.querySelector("#about-heading").innerHTML = aboutHeading;
 
 
+// making an image gallery in js
+// image gallery 
 
-// image gallery
+const images = [
+'assets/gallery/01-greens.jpg',
+// 'assets/gallery/02-jacket.jpg alt="red orange and white crochet jacket"',
+// 'assets/gallery/03-purplehat.jpg alt="purple hat"',
+// 'assets/gallery/04-sock.jpg alt="red and white crochet large sock"',
+// 'assets/gallery/05-babyhat.jpg alt="blue red and yellow baby hat"'
+];
+console.log(images);
+// gets image container
+  const container = document.getElementById('gallery-box');
+// using a loop to go through images i as my iterator
+  for (let i = 0; i < images.length; i++) {
+    // creates image tag in html for each image
+      const img = document.createElement('img');
+      // get image source
+      img.src = images[i];
+      // appends child - adds each image at the end of the array
+      let displayImg = container.appendChild(img).innerHTML;
+  }
 
-const images = {
+  // should display image
+document.querySelector('#gallery-box').innerHTML = displayImg;
 
-}
 
 // list all images in an object as strings
 
@@ -32,4 +52,5 @@ function myNav() {
 }
 
 // connect myNav function to menu icon
-document.querySelector("#nav-toggle").addEventListener("click", myNav)
+document.querySelector("#nav-toggle").addEventListener("click", myNav);
+
